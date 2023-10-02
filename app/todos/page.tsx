1c9d0418262e6d79f0d73aac6db6adaad6535c9d
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-02 11:20:17 -0300

    creation of the todos's type

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TodosPage = () => {
-  return <div>TodosPage</div>;
+  return (
+    <div>
+      <h1>This is where the Todos will be listed</h1>
+    </div>
+  );
 };
 
 export default TodosPage;
