5de760783f33ea58e6fed4daf539d8c3109c9b5b
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-02 11:21:09 -0300

    creation of the todos layout

---
+++ app/todos/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodosList from "./TodosList";
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex">
+      <div>
+        <TodosList />
+      </div>
+      <div className="flex-1">{children}</div>
+    </main>
+  );
+};
+
+export default RootLayout;
