0f8dc816e2f127cf811fff750157778d4bd6a097
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-02 11:17:52 -0300

    add the layout to he index page

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Header = () => {
   return (
     <header className="p-5 bg-blue-500">
-      <p className="font-bold text-white">I am the Header</p>
       <Link href="/" className="px-2 py-1 bg-white text-blue-500">
         Home
       </Link>
